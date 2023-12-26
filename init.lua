5b50c9c94db07d7751395c22a45cbb1c431fbd52
Oki1 <oskar.rotar@gmail.com>
2023-12-26 01:46:35 +0100

    poof changes

--- init.lua
+++ init.lua
@@ -7,9 +7,17 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
-
+vim.opt.smartindent = true
+vim.scrolloff = 15
+vim.opt.clipboard:append{"unnamedplus", "unnamedplus"}
 local lazy = {}
 
+if(vim.loop.os_uname().sysname=="Windows NT") then
+    vim.cmd([[
+    set shell=pwsh
+    set shellcmdflag=-command
+]])
+end
 function lazy.install(path)
   if not vim.loop.fs_stat(path) then
     print('Installing lazy.nvim....')
@@ -42,8 +50,48 @@ lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 lazy.opts = {}
 
 lazy.setup({
-{'folke/tokyonight.nvim'},
+{
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require 'nordic' .load()
+    end
+},
+{'williamboman/mason.nvim'},
+{'williamboman/mason-lspconfig.nvim'},
+{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+{'neovim/nvim-lspconfig'},
+{'hrsh7th/cmp-nvim-lsp'},
+{'hrsh7th/nvim-cmp'},
+{'L3MON4D3/LuaSnip'},
+
 })
 vim.opt.termguicolors = true
-vim.cmd.colorscheme('tokyonight')
+vim.cmd.colorscheme 'nordic'
 
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+require('mason').setup({})
+
+
+
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here 
+  -- with the ones you want to install
+  ensure_installed = {'lua_ls', 'clangd','pyright'},
+  handlers = {
+    lsp_zero.default_setup,
+    },
+})
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- delay update diagnostics
+    update_in_insert = true,
+  }
+)
