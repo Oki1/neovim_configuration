46b5d2694cbcc1f20594022369a65eb732dd0dc5
Oki1 <oskar.rotar@gmail.com>
2024-01-19 19:12:26 +0100

    added stuff

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.ignorecase = true
@@ -8,10 +11,15 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
-vim.scrolloff = 15
-vim.opt.clipboard:append{"unnamedplus", "unnamedplus"}
+vim.opt.scrolloff= 15
+vim.opt.clipboard:append{"unnamedplus", "unnamed"}
+vim.opt.showtabline=0
+vim.g.mapleader = 'ć'
+vim.keymap.set("", "<c-j>", "}")
+vim.keymap.set("", "p", "p <cmd>sil! :%s/\\r//g<cr>")
+vim.keymap.set("", "<C-k>", "{")
+--lazy setup
 local lazy = {}
-
 if(vim.loop.os_uname().sysname=="Windows NT") then
     vim.cmd([[
     set shell=pwsh
@@ -31,33 +39,22 @@ function lazy.install(path)
     })
   end
 end
-
 function lazy.setup(plugins)
   if vim.g.plugins_ready then
     return
   end
-
-  -- You can "comment out" the line below after lazy.nvim is installed
   lazy.install(lazy.path)
 
   vim.opt.rtp:prepend(lazy.path)
-
   require('lazy').setup(plugins, lazy.opts)
   vim.g.plugins_ready = true
 end
-
 lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 lazy.opts = {}
 
+--plugins
 lazy.setup({
-{
-    'AlexvZyl/nordic.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require 'nordic' .load()
-    end
-},
+{ 'rose-pine/neovim', name = 'rose-pine' },
 {'williamboman/mason.nvim'},
 {'williamboman/mason-lspconfig.nvim'},
 {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
@@ -65,26 +62,95 @@ lazy.setup({
 {'hrsh7th/cmp-nvim-lsp'},
 {'hrsh7th/nvim-cmp'},
 {'L3MON4D3/LuaSnip'},
-
+{'m4xshen/autoclose.nvim'},
+{"aserowy/tmux.nvim"},
+{'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' }},
+{'ms-jpq/chadtree', branch='chad', build='python3 -m chadtree deps'},
+{
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    },
+    lazy = false,
+}
+--{'numToStr/Navigator.nvim', }
 })
+
+require('Comment').setup()
+vim.cmd('colorscheme rose-pine-moon')
+--require("Navigator").setup()
+require("autoclose").setup()
+require("tmux").setup({
+    copy_sync={sync_clipboard = false},
+    navigation = {
+        enable_default_keybindings = false,
+    },
+})
+--Navigator
+--vim.keymap.set({'n', 't'}, '<A-a>', '<cmd> tes:t')
+--vim.keymap.set({'n', 't'}, '<A-d>', '<CMD>NavigatorRight<CR>')
+--vim.keymap.set({'n', 't'}, '<A-w>', '<CMD>NavigatorUp<CR>')
+--vim.keymap.set({'n', 't'}, '<A-s>', '<CMD>NavigatorDown<CR>')
+--tmux
+tmux = require("tmux")
+vim.keymap.set({'n', 't'}, '<C-a>', tmux.move_left)
+vim.keymap.set({'n', 't'}, '<C-w>', tmux.move_top)
+vim.keymap.set({'n', 't'}, '<C-s>', tmux.move_bottom)
+vim.keymap.set({'n', 't'}, '<C-d>', tmux.move_right)
+
+local function vspl()
+    vim.cmd("vsplit")
+    tmux.move_right()
+end
+local function hspl()
+    vim.cmd("split")
+end
+
+vim.keymap.set({'n', 't'}, '<C-v>', vspl)
+vim.keymap.set({'n', 't'}, '<C-h>', hspl) 
+vim.keymap.set({'n', 't'}, '<c-x>', vim.cmd.quit)
+local function maximize()
+    local ok, _ = pcall(vim.cmd.tabc)
+    if ok == false then
+        vim.cmd("tab split")
+    end
+end
+vim.keymap.set({'n', 't'}, '<c-z>', maximize)
+
+--Chadtree
+local chadtree_settings = {["view.open_direction"] = "right"}
+vim.api.nvim_set_var("chadtree_settings", chadtree_settings)
+vim.keymap.set({"n", 't'}, "č", vim.cmd.CHADopen)
+--theme setup
 vim.opt.termguicolors = true
-vim.cmd.colorscheme 'nordic'
 
+--telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+--lsp
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+  })
+})
 local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(_client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 require('mason').setup({})
-
-
-
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'clangd','pyright'},
+  select_beheviour = 'insert',
+  ensure_installed = {'lua_ls', 'clangd','pyright', 'yamlls', 'swift_mesonls'},
   handlers = {
     lsp_zero.default_setup,
     },
